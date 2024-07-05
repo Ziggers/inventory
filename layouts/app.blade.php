5729c50a71dfb65edb819ded330fccf26469e9dd
Ziggers <123231732+Ziggers@users.noreply.github.com>
2024-07-05 10:16:36 +0800

    app.blade.php

--- layouts/app.blade.php
+++ layouts/app.blade.php
@@ -5,14 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inventory System</title>
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet">
-    <link href="app.css" rel="stylesheet">
-    </style>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #bda675;">
         <a class="navbar-brand" href="#">Juliana's Farm</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
         <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
+            <ul class="navbar-nav mr-auto">
                 <li class="nav-item">
                     <a class="nav-link" href="{{ route('categories.index') }}">Categories</a>
                 </li>
@@ -20,31 +22,54 @@
                     <a class="nav-link" href="{{ route('product.index') }}">Products</a>
                 </li>
             </ul>
+            <ul class="navbar-nav ml-auto">
+                @guest
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('login') }}">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('register') }}">Register</a>
+                    </li>
+                @else
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('logout') }}"
+                           onclick="event.preventDefault();
+                           document.getElementById('logout-form').submit();">
+                            Logout
+                        </a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
+                    </li>
+                @endguest
+            </ul>
         </div>
     </nav>
 
-<nav class="sidebar">
-    <div class="sidebar-sticky">
-        <ul class="nav flex-column">
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('categories.index') }}">
-                    Categories
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('product.index') }}">
-                    Products
-                </a>
-            </li>
-            <!-- Add more sidebar items as needed -->
-        </ul>
-    </div>
-</nav>
+    <div class="d-flex">
+        <nav class="sidebar">
+            <div class="sidebar-sticky">
+                <ul class="nav flex-column">
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('categories.index') }}">Categories</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('product.index') }}">Products</a>
+                    </li>
+                  
+                </ul>
+            </div>
+        </nav>
 
-    <div class="container">
-        @yield('content')
+        <div class="main-content flex-grow-1">
+            <div class="container">
+                @yield('content')
+            </div>
+        </div>
     </div>
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 </body>
 </html>
-
-<link href="{{ asset('css/app.css') }}" rel="stylesheet">
